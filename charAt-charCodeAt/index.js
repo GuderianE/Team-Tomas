68738de0a10934f4551ec0ba99ba6af9b2e80036
Tomas <eliz.tomas23@gmail.com>
2020-10-21 14:41:47 +0200

    charAt and charCodeAt

---
+++ charAt-charCodeAt/index.js
@@ -0,0 +1,12 @@
+// charAt() - returns a specified UTF-16 character in a string
+// - takes index of a character as an argument
+// - similar use case as str[] and substring()
+
+let str = 'string';
+console.log('a character -->', str.charAt(2)); // r
+
+// charCodeAt() - returns an integer number between 0 and 65535 which      represents a certain UTF - 16 character
+// - takes index of a character as an argument
+
+console.log('a UTF code nr -->', str.charCodeAt(4)) // UTF code for 'n' = 110
+
